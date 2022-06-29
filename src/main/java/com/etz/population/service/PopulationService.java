b7dcde7c1a0566b19d17829dd5a50fa2c7a814a2
David Adenuga <David.Adenuga@etranzact.com>
2022-06-29 13:55:39 +0100

    Changes made to the population response upon a nullable id

--- src/main/java/com/etz/population/service/PopulationService.java
+++ src/main/java/com/etz/population/service/PopulationService.java
@@ -5,6 +5,7 @@ import com.etz.population.repository.PopulationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,9 +33,9 @@ public class PopulationService {
 
     public Optional<Population> findById(Long id){
 
-
       Optional<Population> population = populationRepository.findById(id);
 
-        return population;
+      Population response = population.orElseThrow(() -> new EntityNotFoundException("The id (" + id + ") is not found in the system"));
+        return Optional.ofNullable(response);
     }
 }
